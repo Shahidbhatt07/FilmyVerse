85c5f8681915c562944ed1255d7f631742aa6d86
shahidbhatt07 <bhatshahid6542@gmail.com>
2023-02-14 23:00:39 +0530

    :fire: Styling add new movies section

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import AddIcon from '@mui/icons-material/Add';
 import { Button } from '@mui/material';
+import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
     <div className='text-3xl flex justify-between items-center text-red-500 font-bold p-3 border-b-2 border-gray-500'>
           <span>Filmy<span className='text-white'>Verse</span></span>
-          <h1 className='text-lg flex items-center cursor-pointer'>
-              <Button><AddIcon className='mr-1 ' color='secondary'/><span className='text-white'>Add new</span></Button></h1>
+         <Link to={'/addmovie'}> <h1 className='text-lg flex items-center cursor-pointer'>
+        <Button><AddIcon className='mr-1 ' color='secondary' /><span className='text-white'>Add new</span></Button></h1>
+        </Link>
     </div>
   )
 }
