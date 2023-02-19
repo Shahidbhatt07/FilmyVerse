16c6fdfa1c379ee718920173cbf5516f3f40a25f
shahidbhatt07 <bhatshahid6542@gmail.com>
2023-02-19 14:12:58 +0530

    :art: styling login page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,24 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import AddIcon from '@mui/icons-material/Add';
 import { Button } from '@mui/material';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import { Appstate } from '../App';
 
 const Header = () => {
+  const useAppstate = useContext(Appstate);
+
   return (
     <div className='header sticky z-10 top-0 text-3xl flex justify-between items-center text-red-500 font-bold p-3 border-b-2 border-gray-500'>
           <Link to={'/'}><span>Filmy<span className='text-white'>Verse</span></span></Link>
-         <Link to={'/addmovie'}> <h1 className='text-lg flex items-center cursor-pointer'>
+      {useAppstate.login ?
+        <Link to={'/addmovie'}> <h1 className='text-lg flex items-center cursor-pointer'>
         <Button><AddIcon className='mr-1 ' color='secondary' /><span className='text-white'>Add new</span></Button></h1>
         </Link>
+        :
+        <Link to={'/login'}> <h1 className='text-lg bg-green-500 flex items-center cursor-pointer'>
+        <Button><span className='text-white font-medium capitalize'>Login</span></Button></h1>
+        </Link>
+      }
     </div>
   )
 }
